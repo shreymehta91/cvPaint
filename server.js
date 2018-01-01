0d6877d2334cce4a2bebdbb09481f6ad4d8031ef
Shrey Mehta <shreymehta91@gmail.com>
2018-01-01 15:25:51 +0530

    updating package.json for prod env

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const path = require('path');
 const app = express();
 
 // Log requests to the console.
-app.use(logger('dev'));
+//app.use(logger('dev'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('*', (req, res) => res.sendfile('./public/index.html'));
 
-app.listen(4040);
+app.listen((process.env.PORT || 4040));
 module.exports = app;
